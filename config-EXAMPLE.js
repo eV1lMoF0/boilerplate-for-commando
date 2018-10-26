d7168c0f603736fe6824d6d258fa538be66484d9
Evil Mofo <cygywxqre4sp@opayq.com>
2018-10-26 13:11:05 -0700

    Initial commit

---
+++ config-EXAMPLE.js
@@ -0,0 +1,10 @@
+const config = {
+    // Bot Owner, 
+    "owner": "123456789012345678",
+  
+    // Your Bot's Token. Available on https://discordapp.com/developers/applications/me
+    "token": "Dg1OTg0- NOT -NDM1MzM- A -jc0.Dqf5XA.gJL- TOKEN -s9gD5ojfSZy",
+  };
+  
+  module.exports = config;
+  
